295b17dbf103204285a9fd3c33f3c786e7e9d9dc
Xiaoman Lu <xiaoman@baseline-login4.ccs.ornl.gov>
2025-03-27 16:40:05 -0400

    ensemble analysis and  plot

--- manage_ensemble.py
+++ manage_ensemble.py
@@ -20,7 +20,6 @@ parser.add_option("--case", dest="case", default="", \
 myfile=open('pklfiles/'+options.case+'.pkl','rb')
 mycase=pickle.load(myfile)
 
-mycase.postproc_vars = []
 
 #get the node file and parse
 def get_nodelist():
